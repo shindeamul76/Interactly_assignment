865d31613fcedbea3c198b065f4616f6c6094539
Amul <shindeamul76@gmail.com>
2023-04-30 23:32:32 +0530

    some minor changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 
 import './App.css';
 import Home from './components/Home';
-// import New from './components/New';
 
 
 function App() {
   return (
     <div className='App'>
    <Home/>
-   {/* <New/> */}
     </div>
   );
 }
